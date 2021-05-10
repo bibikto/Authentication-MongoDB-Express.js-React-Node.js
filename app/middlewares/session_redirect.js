46eb7958f996cde2b7776c5c4ccf2ebf360adae5
Bibikto Das <bibikto56@gmail.com>
2021-05-10 22:31:06 +0530

    Fix HTTPS Redirect

--- app/middlewares/session_redirect.js
+++ app/middlewares/session_redirect.js
@@ -1,31 +1,29 @@
-const session = require('express-session')
-require('dotenv').config();
-const MongoStore = require("connect-mongo")
+const session = require("express-session");
+require("dotenv").config();
+const MongoStore = require("connect-mongo");
 
 module.exports = async (app, connection) => {
-    app.enable("trust proxy", 1);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        sameSite: process.env.NODE_ENV == "production",
+        secure: process.env.NODE_ENV == "production",
+        maxAge: 24 * 60 * 60 * 1000,
+      },
+      store: new MongoStore({
+        client: connection.connection.getClient(),
+        clear_interval: 3600,
+      }),
+    })
+  );
 
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            sameSite: process.env.NODE_ENV == "production",
-            secure: process.env.NODE_ENV == "production",
-            maxAge: 24 * 60 * 60 * 1000
-        },
-        store: new MongoStore({
-            client: connection.connection.getClient(),
-            clear_interval: 3600
-        })
-    }))
+  app.use(async (req, res, next) => {
+    if (process.env.NODE_ENV == "production" && !request.secure)
+      return res.redirect("https://" + req.headers.host + req.url);
 
-    if (process.env.NODE_ENV == "production")
-        app.use(async (req, res, next) => {
-            if (req.secure)
-                next();
-            else
-                res.redirect("https://" + req.headers.host + req.url);
-        });
-
-}
+    next();
+  });
+};
