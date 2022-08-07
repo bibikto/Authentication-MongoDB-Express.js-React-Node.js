84ef54e3912e4b7411d132f4a6b6963b56b4182f
Bibikto Das <bibikto56@gmail.com>
2022-08-07 18:12:08 +0530

    - Add theme reducer

---
+++ client/src/redux/Theme/theme.reducer.js
@@ -0,0 +1,50 @@
+import { createTheme } from '@material-ui/core/styles'
+
+if (!localStorage.getItem("themeType"))
+  localStorage.setItem("themeType", "dark");
+
+const backgroundColor = {
+  "light": "white",
+  "dark": "#18191A"
+}
+
+const themeType = localStorage.getItem("themeType");
+
+const INITIAL_STATE = {
+  theme: createTheme({
+    palette: {
+      type: themeType,
+      background: {
+        default: backgroundColor[themeType],
+      }
+    }
+  })
+}
+
+
+
+const reducer = (state = INITIAL_STATE, action = "SWITCH") => {
+  switch (action.type) {
+    case 'SWITCH': {
+      let type = state.theme.palette.type === 'light' ? 'dark' : 'light'
+      localStorage.setItem("themeType", type);
+
+      return {
+        ...state, theme: createTheme({
+          palette: {
+            type: type,
+            background: {
+              default: backgroundColor[type],
+            }
+          },
+
+        }),
+      };
+    }
+    default: {
+      return state
+    }
+  }
+};
+
+export default reducer;
